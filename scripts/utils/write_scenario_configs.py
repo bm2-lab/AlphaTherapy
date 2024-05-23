d715905ca4dba026fab006a926166b6dea900b9a
Xiaohan-Chen <1034144834@qq.com>
2024-05-23 16:16:33 +0800

    Update write_scenario_configs.py

--- scripts/utils/write_scenario_configs.py
+++ scripts/utils/write_scenario_configs.py
@@ -34,6 +34,11 @@ config.read(cell_model_config_file_path, encoding='UTF-8')
 for cell_line in args.cell_line: 
     for terminal_step in args.terminal_step:
         env_name = 'ENV_%s_%s_STEP%d' % (args.drugset_short_name, cell_line, terminal_step)
+
+        if env_name in config:
+            print(f"{env_name} existed! Now the {env_name} will be overwrite!")
+            config.remove_section(env_name)
+
         config.add_section(section=env_name)
         config[env_name]['cell_line'] = cell_line
         config[env_name]['max_step_number'] = str(terminal_step)
@@ -56,6 +61,11 @@ for cell_line in args.cell_line:
 
         for seed in range(1, 11):
             config_name = env_name + "_SEED" + str(seed)
+
+            if config_name in config:
+                print(f"{config_name} existed! Now the {config_name} will be overwrite!")
+                config.remove_section(config_name)
+                
             config.add_section(section=config_name)
             config[config_name]['env name'] = env_name
             config[config_name]['rl seed'] = str(seed)
